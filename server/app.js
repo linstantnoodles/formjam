33d4706c3f6ec0fe622dca8ba076fca5500eff5d
Alan <alin@2u.com>
2014-06-14 14:36:52 -0400

    serve files from node

---
+++ server/app.js
@@ -0,0 +1,25 @@
+var express     = require("express"),
+    bodyParser  = require("body-parser"),
+    serveStatic = require("serve-static");
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+// Parse request bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+// Set static file directory
+app.use(serveStatic(__dirname + "/client"));
+
+// Get forms of template
+// Upload image for template
+// Get form
+// Get template
+
+console.log("Listening to port: " + app.get('port'));
+
+app.listen(app.get('port'));
+
+
