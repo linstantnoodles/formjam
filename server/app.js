908dc78e7061d147f0a023f645bdb54102406a40
Alan <alin@2u.com>
2014-06-14 16:07:36 -0400

    added multer for form uploads

--- server/app.js
+++ server/app.js
@@ -1,24 +1,43 @@
 var express     = require("express"),
     bodyParser  = require("body-parser"),
-    serveStatic = require("serve-static");
+    serveStatic = require("serve-static"),
+    multer      = require("multer");
 
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
 
-// Parse request bodies
+/**
+ * Parse request bodies
+ */
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
+app.use(multer({dest: './form-images'}));
 
-// Set static file directory
+/**
+ * Set static file directory
+ */
+
 app.use(serveStatic("../client"));
 
-// Get forms of template
-// Upload image for template
-// Get form
-// Get template
+/**
+ * Get forms of template
+ */
+
+app.get("template/:id/forms", function(req, res) {
+  console.log("foobar");
+});
+
+/**
+ * Upload image for template
+ */
+
+app.post("form/:id/upload", function(req, res) {
+  console.log(req.body);
+  console.log(req.files);
+});
 
 console.log("Listening to port: " + app.get('port'));
-
 app.listen(app.get('port'));
 
