a8256e3810ea362978cf0db45902ffa4d2b244e2
Alan <alin@2u.com>
2014-06-15 00:27:39 -0400

    add default json for form template

--- client/scripts/model/form-template.js
+++ client/scripts/model/form-template.js
@@ -1,5 +1,25 @@
 "use strict"
 
+/**
+ * Form configuration
+ *
+ * {
+ *  "title": "something"
+ *  "fields": {
+ *      "name": {
+ *        "type": "input",
+ *        "value": "",
+ *        "location": {
+ *          "x": 100,
+ *          "y": 200,
+ *          "h": 300,
+ *          "w": 400
+ *        }
+ *      }
+ *   }
+ * }
+ */
+
 require([
   "backbone",
   "jquery"
@@ -8,7 +28,16 @@ require([
   var FormTemplateModel = Backbone.Model.extend({
 
     defaults: {
-      "title": "Untitled"
+      "title": "Untitled",
+      "config": {
+        "title": "Untitled",
+        "fields": {
+        }
+      }
+    },
+
+    addInputField: function() {
+      console.log("adding in model");
     }
 
   });
