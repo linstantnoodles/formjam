d13fae039cee252244dbfc17b6f3caeb32754bf7
Alan <alin@2u.com>
2014-06-15 00:28:10 -0400

    added basic form adding

---
+++ client/scripts/view/form-template-view/input-element.js
@@ -0,0 +1,49 @@
+"use strict"
+
+/**
+ * View for individual fields
+ */
+
+define([
+  "backbone",
+  "jquery",
+  "underscore",
+  "handlebars",
+  "text!template/form-template-component/input-element.html"
+], function(Backbone, $, _, handlebars, inputElementTemplate) {
+
+  var InputElementView = Backbone.View.extend({
+
+    template: handlebars.compile(inputElementTemplate),
+
+    events: {
+      "click .save-field": "saveInputField"
+    },
+
+    /**
+     * Save an input field
+     */
+
+    saveInputField: function(e) {
+      var $input = this.$("input[type=text]");
+      var fieldName = $input.val();
+      // Remove save button
+      this.$(".save-field").remove();
+      // Update label
+      this.$("label").text(fieldName);
+      // Disable text field
+      this.$("input[type=text]")
+        .val("")
+        .attr("disabled", "disabled");
+    },
+
+    render: function() {
+      this.$el.html(this.template());
+      return this;
+    }
+
+  });
+
+  return InputElementView;
+});
+
